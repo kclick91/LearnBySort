688f602b5cf30fbadd67756cab14c4e3fcf0983a
Kelly Click <kclick91@gmail.com>
2021-11-05 09:01:03 -0400

    Add files via upload

--- LearnBySort/src/LBS.java
+++ LearnBySort/src/LBS.java
@@ -66,13 +66,16 @@ public class LBS {
                 {
                     if (sm[k] == Max && trainingData[j][k] == 1)
                     {
-                        trainingData[j][k] = 0;
+                        trainingData[j][0] = 0;
+                        trainingData[j][1] = 0;
+                        trainingData[j][2] = 0;
                     }
                 }
 
             }
             System.out.println("");
-            int[] in2 = Sum(trainingData);
+            int[] in2 = Sort(Sum(trainingData));
+            int[] sum2 = Sum(trainingData);
             for (int i = 0; i < in2.length; i++)
             {
                 System.out.print(in2[i] + " ");
@@ -80,15 +83,58 @@ public class LBS {
             }
             System.out.println();
             int Max2 = 0;
-            for (int i = 0 ; i < in.length; i++)
+            for (int i = 0 ; i < in2.length; i++)
             {
-                if (in2[i] > Max2)
+                if (sum2[i] > Max2)
                 {
-                    Max2 = in2[i];
+                    Max2 = sum2[i];
                 }
-                System.out.print(i + ":" + in2[i] + " ");
+                System.out.print(i + ":" + sum2[i] + " ");
             }
+            //Remove all arrays with Max as 1 for the 3rd most important value
+            for (int j = 0; j < in2.length; j++)
+            {
+                for (int k = 0; k < in2.length; k++)
+                {
+                    if (sum2[k] == Max2 && trainingData[j][k] == 1)
+                    {
+                        trainingData[j][0] = 0;
+                        trainingData[j][1] = 0;
+                        trainingData[j][2] = 0;
+                    }
+                }
 
+            }
+            System.out.println("");
+            int[] in3 = Sort(Sum(trainingData));
+            int[] sum3 = Sum(trainingData);
+            for (int i = 0; i < in3.length; i++)
+            {
+                System.out.print(in3[i] + " ");
 
+            }
+            System.out.println();
+            int Max3 = 0;
+            for (int i = 0 ; i < in3.length; i++)
+            {
+                if (sum3[i] > Max3)
+                {
+                    Max3 = sum3[i];
+                }
+                System.out.print(i + ":" + sum3[i] + " ");
+            }
+            for (int j = 0; j < in3.length; j++)
+            {
+                for (int k = 0; k < in3.length; k++)
+                {
+                    if (sum3[k] == Max3 && trainingData[j][k] == 1)
+                    {
+                        trainingData[j][0] = 0;
+                        trainingData[j][1] = 0;
+                        trainingData[j][2] = 0;
+                    }
+                }
+
+            }
         }
     }
